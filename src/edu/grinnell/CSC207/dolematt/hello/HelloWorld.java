2d969fb8aaaca7434610f65978343af65332e080
mdole <dolematt@grinnell.edu>
2013-09-04 10:44:11 -0500

    Update HelloWorld.java

--- src/edu/grinnell/CSC207/dolematt/hello/HelloWorld.java
+++ src/edu/grinnell/CSC207/dolematt/hello/HelloWorld.java
@@ -2,6 +2,6 @@ package edu.grinnell.CSC207.dolematt.hello;
 
 public class HelloWorld {
 	   public static void main(String[] args) {
-	        System.out.println("Hello, GitHub!");
+	        System.out.println("Hello, Eclipse!");
 	    } // main(String[])
 }

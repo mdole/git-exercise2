061f131573da92be255d49f168994f4067a109eb
dolematt <dolematt@grinnell.edu>
2013-09-04 10:41:50 -0500

    Hello GitHub file

---
+++ src/edu/grinnell/CSC207/dolematt/hello/HelloWorld.java
@@ -0,0 +1,7 @@
+package edu.grinnell.CSC207.dolematt.hello;
+
+public class HelloWorld {
+	   public static void main(String[] args) {
+	        System.out.println("Hello, GitHub!");
+	    } // main(String[])
+}
